2038ed9ab277e000cf6a6cef873637b0bebff390
gkskaks1004 <gkskaks1008@naver.com>
2017-08-16 16:31:32 +0900

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,22 +1,21 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
+# This is the user-interface definition of a Shiny web application.# You can find out more about building applications with Shiny here:
+## http://shiny.rstudio.com
 library(shiny)
+
 shinyUI(
   fluidPage(
-    theme = "bootstrap.css",
+    #theme = "bootstrap.css",
     headerPanel("The Best Team D&M"),
     #titlePanel("Dong-jin & Mun-hwa Internship Project 2017.8"),
     
+    sidebarLayout(
+      sidebarPanel(
+        tableOutput("dotcom")
+      ),
+  
     mainPanel(
-      tableOutput("dotcom"),
-      tableOutput("naver_shopping")
-    )
+  tableOutput("naver_shopping")
   )
+    )
+)
 )
