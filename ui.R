6ee0c3a9460d6e5bfafd25e5722db047e094608e
gkskaks1004 <gkskaks1008@naver.com>
2017-08-17 21:50:12 +0900

    Update ui.R

--- ui.R
+++ ui.R
@@ -5,17 +5,19 @@ library(shiny)
 shinyUI(
   fluidPage(
     #theme = "bootstrap.css",
-    headerPanel("The Best Team D&M"),
+    headerPanel("동진♥문화의 크롤링 이야기"),
     #titlePanel("Dong-jin & Mun-hwa Internship Project 2017.8"),
-    
-    sidebarLayout(
-      sidebarPanel(
-        tableOutput("dotcom")
-      ),
-  
-    mainPanel(
-  tableOutput("naver_shopping")
-  )
+    sidebarLayout( 
+      tabsetPanel(
+        img(src="com.jpg", width = 431,height =80 ),
+        div(tabPanel("Table", tableOutput("dotcom")), style = 'width:1000px;') 
+      ), 
+      
+      mainPanel(
+        selectInput("var", label = "상품명을 고르세요.", choices = c("1위 상품", "2위 상품", "3위 상품", "4위 상품", "5위 상품", "6위 상품", "7위 상품", "8위 상품", "9위 상품","10위 상품")),
+        img(src="naver.jpg", width = 113, height = 64),
+        tableOutput("naver_shopping")
+      )
     )
-)
+  )
 )
